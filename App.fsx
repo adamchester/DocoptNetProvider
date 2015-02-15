27c3f2e54eec7d3671605879e5ef82c881375bae
Adam Chester <adamchester@gmail.com>
2015-02-15 12:00:00 +1000

    Added a (broken) Apply method,
    which takes the command line arguments (argv) and
    calls docopt.net to parse them. It is broken because
    I do not know how to invoke it corectly.

--- App.fsx
+++ App.fsx
@@ -3,7 +3,9 @@
 
 type theTest = Docopt.DocOptions<"usage: app --myFlag --myString=<myStringValue>">
 
+printfn "%s" ""
 printfn "%s" theTest.DocString
 printfn "%s" theTest.GeneratedCSharp
 
-
+theTest.Apply (System.Environment.GetCommandLineArgs())
+|> printfn "%A"
